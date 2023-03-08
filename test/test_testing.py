fb6a039be11615aa09d756af571b743c9a7d9070
Philip Meier <github.pmeier@posteo.de>
2023-03-08 21:28:04 +0100

    QoL improvements for torch.testing.make_tensor
    ghstack-source-id: 924864be19c5c6d4ebff0358f0e04171e76fd5af
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/96125

--- test/test_testing.py
+++ test/test_testing.py
@@ -1399,7 +1399,9 @@ class TestMakeTensor(TestCase):
             t = make_tensor()
             self.assertEqual(t.requires_grad, requires_grad)
         else:
-            with self.assertRaisesRegex(ValueError, "requires_grad must be False for integral dtype"):
+            with self.assertRaisesRegex(
+                    ValueError, "`requires_grad=True` is not supported for boolean and integral dtypes"
+            ):
                 make_tensor()
 
     @supported_dtypes
@@ -1433,7 +1435,7 @@ class TestMakeTensor(TestCase):
 
     @supported_dtypes
     def test_noncontiguous_memory_format(self, dtype, device):
-        with self.assertRaises(AssertionError):
+        with self.assertRaisesRegex(ValueError, "`noncontiguous` and `memory_format` are mutually exclusive"):
             torch.testing.make_tensor(
                 (2, 3, 4, 5),
                 dtype=dtype,
@@ -1507,7 +1509,7 @@ class TestMakeTensor(TestCase):
             # FIXME: bool needs to fail as well
             return
 
-        with self.assertRaisesRegex(ValueError, "one of low or high was NaN"):
+        with self.assertRaisesRegex(ValueError, "`low` and `high` cannot be NaN"):
             torch.testing.make_tensor(dtype=dtype, device=device, low=low, high=high)
 
     # FIXME
